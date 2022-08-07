da1c78868099574b1228f512bb75c65a6c8f4334
GornDanil <puls300@yandex.ru>
2022-08-07 21:51:28 +0700

    Шаблон

---
+++ database/migrations/2022_08_07_133016_remove_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('role_type', 16);
+            $table->foreign('role_type')->references('access')->on('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('role_type');
+            $table->dropColumn('role_type');
+        });
+    }
+};
